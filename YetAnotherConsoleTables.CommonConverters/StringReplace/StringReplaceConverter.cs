48076a0c8944c838b6efe5d166ec9b69ad33a0be
Yakiv Yusin <yusin.yakiv@gmail.com>
2023-06-05 14:37:16 +0300

    Add String Replace Converter

---
+++ YetAnotherConsoleTables.CommonConverters/StringReplace/StringReplaceConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace YetAnotherConsoleTables.CommonConverters.StringReplace
+{
+    public class StringReplaceConverter : TableMemberConverter
+    {
+        private readonly string _oldValue;
+        private readonly string _newValue;
+
+        public StringReplaceConverter(string oldValue, string newValue)
+        {
+            _oldValue = oldValue;
+            _newValue = newValue;
+        }
+
+        public override bool CanConvert(Type objectType) => true;
+
+        public override string Convert(object value) => value?.ToString().Replace(_oldValue, _newValue);
+    }
+}
