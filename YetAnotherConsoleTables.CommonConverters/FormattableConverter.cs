4a117dd2b973340f9282c3591ef5976531cb0178
Yakiv Yusin <yusin.yakiv@gmail.com>
2023-06-05 13:55:13 +0300

    Add Formattable Converter

---
+++ YetAnotherConsoleTables.CommonConverters/FormattableConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace YetAnotherConsoleTables.CommonConverters
+{
+    public class FormattableConverter : TableMemberConverter<IFormattable>
+    {
+        private readonly string _format;
+        private readonly CultureInfo _culture;
+
+        public FormattableConverter(string format) : this(format, null)
+        {
+        }
+
+        public FormattableConverter(string format, string cultureCode)
+        {
+            _format = format;
+
+            if (cultureCode != null)
+            {
+                _culture = CultureInfo.GetCultureInfo(cultureCode);
+            }
+        }
+
+        public override string Convert(IFormattable value) => value.ToString(_format, _culture);
+    }
+}
