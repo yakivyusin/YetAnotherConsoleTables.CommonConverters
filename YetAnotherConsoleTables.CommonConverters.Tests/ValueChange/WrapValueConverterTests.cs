91d3c5ccf5aaf9698240dcf7996fe610fdcd4e9a
Yakiv Yusin <yusin.yakiv@gmail.com>
2023-06-05 14:25:41 +0300

    Add Wrap Value Converter (Prepend + Append)

---
+++ YetAnotherConsoleTables.CommonConverters.Tests/ValueChange/WrapValueConverterTests.cs
@@ -0,0 +1,40 @@
+﻿using YetAnotherConsoleTables.CommonConverters.ValueChange;
+
+namespace YetAnotherConsoleTables.CommonConverters.Tests.ValueChange
+{
+    public class WrapValueConverterTests : BaseTests
+    {
+        private class WrapStringWithStringAndInt
+        {
+            [TableMemberConverter<WrapValueConverter>(ConstructorArgs = new object[] { "prefix:", 1 })]
+            public string Value { get; set; }
+        }
+
+        private class WrapIntWithStringAndString
+        {
+            [TableMemberConverter<WrapValueConverter>(ConstructorArgs = new object[] { "prefix:", ":suffix" })]
+            public int Value { get; set; }
+        }
+
+        [Theory]
+        [InlineData(null, "| prefix:1 |")]
+        [InlineData("", "| prefix:1 |")]
+        [InlineData("test", "| prefix:test1 |")]
+        public void WrapStringWithStringAndIntTest(string value, string expected)
+        {
+            ConsoleTable.From(new[] { new WrapStringWithStringAndInt { Value = value } }).Write(_writer);
+
+            Assert.Equal(expected, ValueLine);
+        }
+
+        [Theory]
+        [InlineData(0, "| prefix:0:suffix |")]
+        [InlineData(1000, "| prefix:1000:suffix |")]
+        public void WrapIntWithStringAndStringTest(int value, string expected)
+        {
+            ConsoleTable.From(new[] { new WrapIntWithStringAndString { Value = value } }).Write(_writer);
+
+            Assert.Equal(expected, ValueLine);
+        }
+    }
+}
---
+++ YetAnotherConsoleTables.CommonConverters/ValueChange/WrapValueConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace YetAnotherConsoleTables.CommonConverters.ValueChange
+{
+    public class WrapValueConverter : TableMemberConverter
+    {
+        private readonly object _prefix;
+        private readonly object _suffix;
+
+        public WrapValueConverter(object prefix, object suffix)
+        {
+            _prefix = prefix;
+            _suffix = suffix;
+        }
+
+        public override bool CanConvert(Type objectType) => true;
+
+        public override string Convert(object value) => $"{_prefix}{value}{_suffix}";
+    }
+}
