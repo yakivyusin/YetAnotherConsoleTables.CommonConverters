b1f4b7edee571f64381cd7d0acfdfd5153130c85
Yakiv Yusin <yusin.yakiv@gmail.com>
2023-06-05 14:53:24 +0300

    Add Regex Replace Converter

---
+++ YetAnotherConsoleTables.CommonConverters.Tests/StringReplace/RegexReplaceConverterTests.cs
@@ -0,0 +1,24 @@
+﻿using YetAnotherConsoleTables.CommonConverters.StringReplace;
+
+namespace YetAnotherConsoleTables.CommonConverters.Tests.StringReplace
+{
+    public class RegexReplaceConverterTests : BaseTests
+    {
+        private class StringReplace
+        {
+            [TableMemberConverter<RegexReplaceConverter>(ConstructorArgs = new object[] { "\\d", "-" })]
+            public string Value { get; set; }
+        }
+
+        [Theory]
+        [InlineData(null, "|       |")]
+        [InlineData("", "|       |")]
+        [InlineData("test0", "| test- |")]
+        public void StringReplaceTest(string value, string expected)
+        {
+            ConsoleTable.From(new[] { new StringReplace { Value = value } }).Write(_writer);
+
+            Assert.Equal(expected, ValueLine);
+        }
+    }
+}
---
+++ YetAnotherConsoleTables.CommonConverters/StringReplace/RegexReplaceConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace YetAnotherConsoleTables.CommonConverters.StringReplace
+{
+    public class RegexReplaceConverter : TableMemberConverter
+    {
+        private readonly string _pattern;
+        private readonly string _replacement;
+
+        public RegexReplaceConverter(string pattern, string replacement)
+        {
+            _pattern = pattern;
+            _replacement = replacement;
+        }
+
+        public override bool CanConvert(Type objectType) => true;
+
+        public override string Convert(object value) => value == null ?
+            null :
+            Regex.Replace(value.ToString(), _pattern, _replacement);
+    }
+}
