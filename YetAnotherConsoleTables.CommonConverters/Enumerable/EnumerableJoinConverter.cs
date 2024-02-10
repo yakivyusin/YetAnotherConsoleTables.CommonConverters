c9ba93be8ec84f3192e21ba9d2ad8149febaa45d
Yakiv Yusin <yusin.yakiv@gmail.com>
2024-02-10 16:08:40 +0200

    Add Enumerable.Join Converter

---
+++ YetAnotherConsoleTables.CommonConverters/Enumerable/EnumerableJoinConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace YetAnotherConsoleTables.CommonConverters.Enumerable
+{
+    /// <summary>
+    /// Joins the original IEnumerable&lt;<typeparamref name="T"/>&gt; using the passed <see langword="string"/> separator.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class EnumerableJoinConverter<T> : TableMemberConverter<IEnumerable<T>>
+    {
+        private readonly string _separator;
+
+        /// <summary>
+        /// Initializes the converter.
+        /// </summary>
+        public EnumerableJoinConverter(string separator) => _separator = separator;
+
+        /// <inheritdoc />
+        public override string Convert(IEnumerable<T> value) => string.Join(_separator, value);
+    }
+}
